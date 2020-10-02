ad32cb058d3ff5e949c049d496fe4b3a3c36c1ca
Papunk <pedrocpt2001@gmail.com>
2020-10-02 15:32:42 -0400

    Vastly improved documentation

--- Timer.py
+++ Timer.py
@@ -4,6 +4,17 @@ import Errors
 class Timer:
 
     def __init__(self, name, timeElapsed, initialTime, area):
+        '''
+        Desc:
+            This class defines the properties and functions needed to run a time tracker
+        
+        Arguments:
+            name (string) - the name of the timer
+            timeElapsed (int) - this is the amount of time the timer has tracked
+            initialTime (int) - if the timer is active, this is the time when it was first set to run
+            area (string) - the name of the area that the timer belongs to
+        '''
+
         self.name = name
         self.timeElapsed = timeElapsed
         self.initialTime = initialTime
@@ -15,20 +26,32 @@ class Timer:
         
 
     def startTimer(self):
-        '''gets the system nanotime'''
+        '''
+        Desc:
+            Saves the system nanotime and starts timer
+        '''
+
         self.initialTime = time.time()
         self.isActive = True
 
 
     def endTimer(self):
-        '''gets the time elapsed since the timer was started as an integer in seconds'''
+        '''
+        Desc:
+            Saves the time elapsed since the timer was started as an integer in seconds stops the timer
+        '''
+
         self.timeElapsed += (time.time() - self.initialTime)
         self.initialTime = 0 
         self.isActive = False
 
 
     def getTimeElapsed(self):
-        '''returns time elapsed without stopping the timer'''
+        '''
+        Desc:
+            Returns time elapsed without stopping the timer
+        '''
+
         if self.isActive:
             self.timeElapsed += (time.time() - self.initialTime)
             self.initialTime = time.time()
@@ -44,11 +67,12 @@ class Timer:
             Adds input value to the time elapsed
 
         Arguments:
-            time - the time in seconds or as a string in HH:MM:SS format
+            time (int, string) - the time in seconds or in HH:MM:SS format
 
         Raises:
             InvalidTimeFormatError
         '''
+
         try: # time in seconds
             time = int(time)
             self.timeElapsed += time
@@ -66,11 +90,12 @@ class Timer:
             changes the time elapsed to the input value
         
         Arguments:
-            time - the time in seconds or as a string in HH:MM:SS format
+            time (int, string) - the time in seconds or in HH:MM:SS format
 
         Raises:
             InvalidTimeFormatError
         '''
+
         try:
             time = int(time)
             self.timeElapsed = time
@@ -82,43 +107,72 @@ class Timer:
                 self.timeElapsed = time
 
 
-    def stringToTime(self, string):
-        '''turns a string in
-        HH:MM:SS format into seconds'''
-        sep1 = string.find(':')
-        sep2 = string.find(':', sep1 + 1)
+    def stringToTime(self, time):
+        '''
+        Desc:
+            Turns a string in HH:MM:SS format into seconds
+
+        Arguments:
+            time (string) - the time in HH:MM:SS format
+        
+        Returns:
+            int
+
+        Raises:
+            InvalidTimeFormatError
+        '''
+
+        sep1 = time.find(':')
+        sep2 = time.find(':', sep1 + 1)
         if sep1 == -1 or sep2 == -2:
-            return None
+            raise Errors.InvalidTimeFormat
         else:
             try:
-                h = int(string[:sep1])
-                m = int(string[sep1+1:sep2])
-                s = int(string[sep2+1:])
+                h = int(time[:sep1])
+                m = int(time[sep1+1:sep2])
+                s = int(time[sep2+1:])
             except:
-                return None
+                raise Errors.InvalidTimeFormat
             if h < 0 or m >= 60 or m < 0 or  s >= 60 or s < 0:
-                return None
+                raise Errors.InvalidTimeFormat
             else:
                 return (h * 3600) + (m * 60) + s
 
 
-    def timeToString(self, s):
-        '''turns time in seconds
-        into a string in HH:MM:SS format'''
+    def timeToString(self, time):
+        '''
+        Desc:
+            Turns time in seconds into a string in HH:MM:SS format
+            
+        Arguments:
+            time (int) - time in seconds as an integer
+
+        Return:
+            string in HH:MM:SS format
+        '''
+
+        try:
+            time = int(time)
+        except ValueError:
+            raise Errors.InvalidTimeFormat
+
+        if time < 0:
+           raise Errors.InvalidTimeFormat
+
         if self.isActive:
             placeHol = '00'
         else:
             placeHol = '--'
 
-        if s > 3600:
+        if time > 3600:
             h = str(s // 3600)
             m = str(int(s % 3600) // 60)
             s = str(int((s % 3600) % 60))
-        elif s > 60:
+        elif time > 60:
             h = placeHol
             m = str(s // 60)
             s = str(int(s % 60))
-        elif s > 0:
+        elif time > 0:
             h = placeHol
             m = placeHol
             s = str(s)
